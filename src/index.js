419a06ed5d296e8fae743517def3331c99723138
sungyun6789 <67104761+sungyun6789@users.noreply.github.com>
2020-12-08 20:48:28 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,16 +6,14 @@ import { BrowserRouter } from 'react-router-dom';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import App from './App';
 import rootReducer from './modules';
-import ResetStyles from './resetStyle/ResetStyles';
 
 const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
-  <>
-    <ResetStyles />
-    <BrowserRouter>
+  <BrowserRouter>
+    <Provider store={store}>
       <App />
-    </BrowserRouter>
-  </>,
+    </Provider>
+  </BrowserRouter>,
   document.getElementById('root'),
 );
