b2c2dc4fb4399580c1fc3d38e14a79eddca2860f
stjae <go961031>
2020-12-17 02:22:33 +0900

    [feat] 뒤로 가기 기능 추가

--- src/components/neighborhood/Header.js
+++ src/components/neighborhood/Header.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import * as Styled from '../common/neighbor/Header';
 import searchLocation from './searchLocation';
-import { inputAddress } from '../../modules/neighbor';
+import { inputAddress, keepAddress } from '../../modules/neighbor';
 
 const Header = () => {
   const [inputText, setInputText] = useState('');
@@ -38,10 +38,19 @@ const Header = () => {
     onSearchAddress('', false);
   };
 
+  const history = useHistory();
+  const mainPage = () => history.push('/');
+
   return (
     <Styled.Wrapper>
       <Link to="/">
-        <Styled.ArrowBackIcon />
+        <Styled.ArrowBackIcon
+          onClick={() => {
+            mainPage();
+            keepAddress();
+            onReset();
+          }}
+        />
       </Link>
       <Styled.SearchBarWrapper>
         <Styled.SearchIcon />
